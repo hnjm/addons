4bf4872645f88fba7e684c8886fea1757bfef123
trantrangtech <81285978+trantrangtech@users.noreply.github.com>
2022-04-16 16:22:16 +0700

    Constraint production quantity according to BOM

---
+++ custom_mrp_bom/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018-2020 Akretion France (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CUSTOM MRP BOM",
+    "version": "15E",
+    "category": "MRP MANAGEMENT",
+    "license": "AGPL-3",
+    "summary": "",
+    "author": "Tran Trang",
+    "depends": ["base", "mrp"],
+    "data": [
+        ],
+    "installable": True,
+}

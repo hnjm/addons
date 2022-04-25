54a823746d3ef73d502bea8cbeb25dbfe34ad19a
trantrangtech <81285978+trantrangtech@users.noreply.github.com>
2022-04-25 11:04:06 +0700

    fix product_import_image
    Add 2 field product_model ,pdt_operation
    Edit logic fuction button_import

--- product_import_image/wizards/product_import_image_wizard.py
+++ product_import_image/wizards/product_import_image_wizard.py
@@ -14,14 +14,15 @@ class ProductImportImageWizard(models.TransientModel):
     _description = u'Product Import Image'
 
     file = fields.Binary(u'Zip File', required=True)
-
+    product_model = fields.Selection([('1', 'Product Template'), ('2', 'Product Variants')], string="Product Model")
+    pdt_operation = fields.Selection([('1', 'Product Creation'), ('2', 'Product Updation')], string="Product Operation")
    
     def button_import(self):
         self.ensure_one()
         zip_data = base64.decodestring(self.file)
         fp = BytesIO()
         fp.write(zip_data)
-
+    
         if not fp:
             raise Exception(_("No file sent."))
         if not zipfile.is_zipfile(fp):
@@ -33,10 +34,23 @@ class ProductImportImageWizard(models.TransientModel):
                     raise UserError(_("File '%s' exceed maximum allowed file size") % zf.filename)
 
                 (name, extension) = os.path.splitext(zf.filename)
-                product_ids = self.env['product.product'].search(
-                    ['|', ('default_code', '=', name), ('barcode', '=', name)])
-                if product_ids:
-                    product_id = product_ids[0]
-                    data = z.read(zf.filename)
+                product_obj = self.env['product.product']
+                if self.product_model == '1':
+                    product_obj = self.env['product.template']
+                data = z.read(zf.filename)
+                vals = {
+                        'name':name,
+                        'image_1920':base64.b64encode(data)
+                    }
+                product_id = product_obj.search(
+                    ['|','|',('name','=',name),('default_code', '=', name), ('barcode', '=', name)],limit=1)
 
-                    product_id.image_1920 = base64.b64encode(data)
+                if self.pdt_operation == '1' and not product_id:
+                    product_obj.create(vals)
+                elif self.pdt_operation == '1' and product_id:
+                    product_id.write(vals)
+                elif self.pdt_operation == '2' and product_id:
+                    product_id.write(vals)
+                elif not product_id and self.pdt_operation == '2':
+                    raise UserError("Could not find the product '%s'" % name)
+        

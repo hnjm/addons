b7806fcef615957be01c7b61cb8b158a413006c3
unknown <duytuongpham123@gmail.com>
2022-05-19 11:21:58 +0700

    fix report

--- report_quotation_template/models/sale_order.py
+++ report_quotation_template/models/sale_order.py
@@ -14,13 +14,37 @@ class SaleOrder(models.Model):
         if total:
             return total['amount_total']
 
-    def validity_quotation(self):
-        if self.validity_date and self.date_order:
-            validity_date = datetime.strptime(str(self.validity_date), '%Y-%m-%d')
-            date_order = datetime.strptime(str(self.date_order), '%Y-%m-%d %H:%M:%S')
-            res = validity_date.day - date_order.day
-            return res
-        return True
+    def get_employee_salesperson(self):
+        partner = self.env['res.users'].browse(self.user_id.id).partner_id
+        return partner.name
+
+    def get_current_week_of_year(self):
+        date = datetime.now()
+        return date.isocalendar()[1]
+
+    def format_date(self, date):
+        if date:
+            res = str(date).split('-')
+            return '{}-{}-{}'.format(res[2], res[1], res[0])
+
+    def format_currency(self, number):
+        currency = "${:,.2f}".format(number)
+        return currency
+
+
+class SaleOderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    def format_currency(self, number):
+        currency = "${:,.2f}".format(number)
+        return currency
+
+    # def validity_quotation(self):
+    #     if self.validity_date and self.date_order:
+    #         validity_date = datetime.strptime(str(self.validity_date), '%Y-%m-%d')
+    #         date_order = datetime.strptime(str(self.date_order), '%Y-%m-%d %H:%M:%S')
+    #         res = validity_date.day - date_order.day
+    #         return res
 
 
 

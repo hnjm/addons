18651fa981606b61afd6cee931fe61ddefe0d6a7
trantrangtech <81285978+trantrangtech@users.noreply.github.com>
2022-05-19 22:18:04 +0700

    template invoice

---
+++ report_invoice_template/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018-2020 Akretion France (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "REPORT INVOICE TEMPLATE",
+    "version": "15E",
+    "category": "ACCOUNT",
+    "license": "AGPL-3",
+    "summary": "",
+    "author": "Tran Trang",
+    "depends": ["account"],
+    "data": [
+        'reports/report_invoices.xml',
+
+    ],
+    "installable": True,
+}

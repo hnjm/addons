9ccef879cd3af194dc6dfd61c43f8eafe981956e
trantrangtech <81285978+trantrangtech@users.noreply.github.com>
2022-05-21 07:52:48 +0700

    template order and add attachment invoice

---
+++ report_order_confirm/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018-2020 Akretion France (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "REPORT ORDER CONFIRMTATION TEMPLATE",
+    "version": "15E",
+    "category": "SALE",
+    "license": "AGPL-3",
+    "summary": "",
+    "author": "Tran Trang",
+    "depends": ["sale", "sale_management"],
+    "data": [
+        'reports/report_order_confirmation.xml',
+
+    ],
+    "installable": True,
+}

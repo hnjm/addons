a37073420a0d94415d24ad6cfff403829f1ede01
trantrangtech <81285978+trantrangtech@users.noreply.github.com>
2022-05-26 03:47:00 +0700

    template report purchase

---
+++ report_purchase/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018-2020 Akretion France (http://www.akretion.com)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "REPORT PURCHASE TEMPLATE",
+    "version": "15E",
+    "category": "PURCHASE",
+    "license": "AGPL-3",
+    "summary": "",
+    "author": "Tran Trang",
+    "depends": ["purchase",'report_template_custom'],
+    "data": [
+        'reports/report_po.xml',
+
+    ],
+    "installable": True,
+}
